c90993c0a7ccf6fda6f1d9ea72dcdd57fc26d763
Nikhil Tripathy <nikhilt231@gmail.com>
2021-05-26 22:16:17 +0530

    Two stack todo

---
+++ src/nikhil/tripathy/arrays/TwoStack.java
@@ -0,0 +1,15 @@
+package nikhil.tripathy.arrays;
+
+public class TwoStack {
+	
+	int size;
+	int arr[] = new int[100];
+	int head1;
+	int head2;
+	
+	public TwoStack() {
+		size = 100;
+		head1 = -1;
+		head2 = size;
+	}
+}
